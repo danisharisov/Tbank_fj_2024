29a703f0380975a1e9393105dc69c63dfc8db212
Danis <danisharisov@gmail.com>
2024-09-21 21:01:24 +0300

    homework 5 initial commit

---
+++ src/main/java/com/example/fj_2024_lesson_5/Fj2024Lesson5Application.java
@@ -0,0 +1,13 @@
+package com.example.fj_2024_lesson_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fj2024Lesson5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fj2024Lesson5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fj_2024_lesson_5/Fj2024Lesson5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fj_2024_lesson_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fj2024Lesson5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
