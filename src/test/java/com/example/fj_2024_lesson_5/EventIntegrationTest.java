c05518e65113eb4f4b3a3bc03430c631c3d8146d
Danis <danisharisov@gmail.com>
2024-11-13 11:14:02 +0300

    Homework 14 check fix

--- src/test/java/com/example/fj_2024_lesson_5/EventIntegrationTest.java
+++ src/test/java/com/example/fj_2024_lesson_5/EventIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
@@ -18,6 +19,7 @@ import static org.junit.Assert.*;
 
 @SpringBootTest
 @Testcontainers
+@ContextConfiguration(initializers = { PostgreSQLInitializer.class })
 public class EventIntegrationTest {
 
     @Autowired
--- src/test/java/com/example/fj_2024_lesson_5/LocationIntegrationTest.java
+++ src/test/java/com/example/fj_2024_lesson_5/LocationIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
@@ -15,6 +16,7 @@ import static org.junit.Assert.*;
 
 @SpringBootTest
 @Testcontainers
+@ContextConfiguration(initializers = { PostgreSQLInitializer.class })
 public class LocationIntegrationTest {
 
     @Autowired
